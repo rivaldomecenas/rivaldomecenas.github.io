25697dcfa8b46ccfee1f3a9feb8c5250a4e099aa
Rivaldo Mecenas <rivaldoccpro@gmail.com>
2019-05-27 15:17:38 -0300

    Personal page to Github.io

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="js/bootstrap.min.js">
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Cute+Font|Merriweather+Sans&display=swap" rel="stylesheet"> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rivaldo Mecenas</title>
+
+</head>
+<body class="text-center">
+
+    <main class="card">
+        <div class="content_card">
+            <img src="img/picture.jpg" alt="">
+            <h2 class="name">Rivaldo Mecenas</h2>
+            <p>#programing</p>
+            <p>#phpprogrammer</p>
+            <p>#webdevelopment</p>
+            <div clas="social_network">
+                <ul class="social_network">
+                    <li><a href="https://github.com/rivaldomecenas"><img src="img/icons/github.png"></a></li>
+                    <li><a href="https://www.linkedin.com/in/rivaldo-barros-mecenas-junior-15b362134/"><img src="img/icons/linkedin.png"></a></li>
+                    <li><a href="https://www.facebook.com/rivaldomecenasjunior"><img src="img/icons/facebook.png"></a></li>
+                    <li><a href="https://www.instagram.com/rivaldomecenas/?hl=pt-br"><img src="img/icons/instagram.png"></a></li>
+                    <p> Social Network </p>
+                </ul>
+            </div>
+        </div>
+        <div class="flags_language">
+            <p class="name">Web Developer :</p>
+            <ul class="flags_language">
+                <li><img src="img/icons/html.png"></li>
+                <li><img src="img/icons/css.png"></li>
+                <li><img src="img/icons/javascript.png"></li>
+                <li><img src="img/icons/php.png"></li>
+            </ul>
+        </div>
+    </main>
+    
+</body>
+</html>
